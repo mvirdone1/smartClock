e5a9fa2cab73e7fcc9df35a441e0181e7160a596
Michael Virdone <mvirdone@gmail.com>
2018-08-29 18:12:51 -0400

    initial baseline

---
+++ smartClock.ino
@@ -0,0 +1,23 @@
+// Required for network connectivity
+#include <ESP8266WiFi.h>
+
+
+// Most importantly needs to define the 'ssid' and 'password' variables for connecting to your local network
+#include "UserNetworkSettings.h"
+
+
+
+
+void setup()
+{
+
+  /* add setup code here */
+
+}
+
+void loop()
+{
+
+  /* add main program code here */
+
+}
