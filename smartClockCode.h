0a7034c12cc43c0e7cea218bf05bb76df870d95e
Michael Virdone <mvirdone@gmail.com>
2018-09-02 10:01:33 -0400

    Significant re-write
    Re-structured all of the code because the arduino library wasn't handling the 6288 library and and my functions during linking. Needed to re-structure to an empty .ino and actual c++ and header files.
    
    The header file isn't very clean, but it compiles and runs the way it is supposed to.

---
+++ smartClockCode.h
@@ -0,0 +1,15 @@
+// smartClockCode.h
+
+#ifndef _SMARTCLOCKCODE_h
+#define _SMARTCLOCKCODE_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
+
